5e885402b2ce4a09c3291c040fb8f5c19977d7c4
Kiber Men <sasapoleckij@gmail.com>
2021-03-29 20:29:10 +0300

    Example 3 completed

---
+++ 4 - DirectX 10/03 - Sprite Output/Common_Defs.h
@@ -0,0 +1,118 @@
+#pragma once
+
+// =============================================================================
+//
+// Warning control
+//
+// =============================================================================
+
+//#pragma  warning(disable: 5045)
+//#pragma  warning(disable: 28251)
+
+#define  THIRD_PARTY_START                                                  \
+    __pragma( warning(push) )                                               \
+    __pragma( warning(disable: 5039) )                                      \
+    __pragma( warning(disable: 4668) )                                      \
+    __pragma( warning(disable: 5039) )                                      \
+    __pragma( warning(disable: 4005) )                                      \
+    __pragma( warning(disable: 5029) )                                      \
+    __pragma( warning(disable: 4820) )
+
+#define  THIRD_PARTY_END                                                    \
+    __pragma( warning(pop) )
+
+
+// =============================================================================
+//
+// Simple types
+//
+// =============================================================================
+
+#include  <cstdint>
+
+
+using  uint8        = std::uint8_t;
+using  uint16       = std::uint16_t;
+using  uint32       = std::uint32_t;
+using  uint64       = std::uint64_t;
+
+using  int8         = std::int8_t;
+using  int16        = std::int16_t;
+using  int32        = std::int32_t;
+using  int64        = std::int64_t;
+
+using  char8        = char8_t;
+using  char16       = char16_t;
+using  char32       = char32_t;
+
+using  sint         = std::intptr_t;
+using  uint         = std::uintptr_t;
+
+using  pvoid        =                void *;
+using  pcvoid       = const          void *;
+using  pvvoid       =       volatile void *;
+using  pcvvoid      = const volatile void *;
+
+using  pint8        =                int8  *;
+using  pcint8       = const          int8  *;
+using  pvint8       =       volatile int8  *;
+using  pcvint8      = const volatile int8  *;
+using  pint16       =                int16 *;
+using  pcint16      = const          int16 *;
+using  pvint16      =       volatile int16 *;
+using  pcvint16     = const volatile int16 *;
+using  pint32       =                int32 *;
+using  pcint32      = const          int32 *;
+using  pvint32      =       volatile int32 *;
+using  pcvint32     = const volatile int32 *;
+using  pint64       =                int64 *;
+using  pcint64      = const          int64 *;
+using  pvint64      =       volatile int64 *;
+using  pcvint64     = const volatile int64 *;
+
+using  puint8       =                uint8  *;
+using  pcuint8      = const          uint8  *;
+using  pvuint8      =       volatile uint8  *;
+using  pcvuint8     = const volatile uint8  *;
+using  puint16      =                uint16 *;
+using  pcuint16     = const          uint16 *;
+using  pvuint16     =       volatile uint16 *;
+using  pcvuint16    = const volatile uint16 *;
+using  puint32      =                uint32 *;
+using  pcuint32     = const          uint32 *;
+using  pvuint32     =       volatile uint32 *;
+using  pcvuint32    = const volatile uint32 *;
+using  puint64      =                uint64 *;
+using  pcuint64     = const          uint64 *;
+using  pvuint64     =       volatile uint64 *;
+using  pcvuint64    = const volatile uint64 *;
+
+using  psint        =                sint *;
+using  pcsint       = const          sint *;
+using  pvsint       =       volatile sint *;
+using  pcvsint      = const volatile sint *;
+
+using  puint        =                uint *;
+using  pcuint       = const          uint *;
+using  pvuint       =       volatile uint *;
+using  pcvuint      = const volatile uint *;
+
+using  pchar        =                char *;
+using  pcchar       = const          char *;
+using  pvchar       =       volatile char *;
+using  pcvchar      = const volatile char *;
+
+using  pchar8       =                char8 *;
+using  pcchar8      = const          char8 *;
+using  pvchar8      =       volatile char8 *;
+using  pcvchar8     = const volatile char8 *;
+
+using  pchar16      =                char16 *;
+using  pcchar16     = const          char16 *;
+using  pvchar16     =       volatile char16 *;
+using  pcvchar16    = const volatile char16 *;
+
+using  pchar32      =                char32 *;
+using  pcchar32     = const          char32 *;
+using  pvchar32     =       volatile char32 *;
+using  pcvchar32    = const volatile char32 *;
---
+++ 4 - DirectX 10/03 - Sprite Output/Main.cpp
@@ -0,0 +1,437 @@
+#include "Common_Defs.h"
+
+THIRD_PARTY_START
+
+#include  <Windows.h>
+#include  <exception>
+#include  <DXGI1_6.h>
+#include  <D3D10_1.h>
+#include  <D3DX10.h>
+
+THIRD_PARTY_END
+
+#pragma comment(lib, "DXGI")
+#pragma comment(lib, "D3D10_1")
+
+constexpr uint32  Render_Width  = 640;
+constexpr uint32  Render_Height = 480;
+
+static const wchar_t  File_Name[] = L"Picture.bmp";
+
+HWND                     g_hWnd;
+
+IDXGIFactory7           *g_pIDXGI_Factory       = nullptr;
+IDXGIAdapter4           *g_pDXGI_Adapter        = nullptr;
+ID3D10Device1           *g_pD3D_Device          = nullptr;
+IDXGISwapChain1         *g_pSwap_Chain          = nullptr;
+ID3D10ShaderResourceView   *g_pShader_Resource      = nullptr;
+ID3DX10Sprite              *g_pSprite               = nullptr;
+
+// =============================================================================
+//
+// Safe_Release
+//
+// =============================================================================
+
+template< typename  T >
+void  Safe_Release(T*  &Ptr)
+{
+    if ( Ptr != nullptr )
+    {
+        Ptr->Release();
+        Ptr = nullptr;
+    }
+}
+
+
+// =============================================================================
+//
+// CHECK_HR
+//
+// =============================================================================
+
+static inline void  CHECK_HR(HRESULT  HR)
+{
+    if ( FAILED(HR) )
+    {
+        throw  std::exception();
+    }
+}
+
+
+// =============================================================================
+//
+// Wnd_Proc
+//
+// =============================================================================
+
+LRESULT CALLBACK  Wnd_Proc(
+    HWND    hWnd,
+    UINT    Message,
+    WPARAM  wParam,
+    LPARAM  lParam)
+{
+    switch ( Message )
+    {
+        case  WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        default:
+            break;
+    }
+
+    return DefWindowProcW(hWnd, Message, wParam, lParam);
+}
+
+
+// =============================================================================
+//
+// Init_Window
+//
+// =============================================================================
+
+void  Init_Window(
+    HINSTANCE  hInstance,
+    int        nCmdShow)
+{
+    constexpr wchar_t  Class_Name[] = L"DX10_Test_Window_Class";
+
+    WNDCLASSEXW  WC;
+
+    WC.cbSize           = sizeof(WNDCLASSEXW);
+    WC.style            = CS_HREDRAW | CS_VREDRAW;
+    WC.lpfnWndProc      = Wnd_Proc;
+    WC.cbClsExtra       = 0;
+    WC.cbWndExtra       = 0;
+    WC.hInstance        = hInstance;
+    WC.hIcon            = NULL;
+    WC.hCursor          = LoadCursorW(NULL, IDC_ARROW);
+    WC.hbrBackground    = reinterpret_cast<HBRUSH>(COLOR_WINDOW);
+    WC.lpszMenuName     = nullptr;
+    WC.lpszClassName    = Class_Name;
+    WC.hIconSm          = LoadIconW(NULL, IDI_APPLICATION);
+
+    if ( !RegisterClassEx(&WC) )
+    {
+        throw std::exception();
+    }
+
+    RECT  Rect = { 0, 0, Render_Width, Render_Height };
+
+    AdjustWindowRect(&Rect, WS_OVERLAPPEDWINDOW, false);
+
+    g_hWnd = CreateWindowW(
+        Class_Name,
+        L"DirectX 10: Вывод текста",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        Rect.right - Rect.left,
+        Rect.bottom - Rect.top,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    if ( g_hWnd == NULL )
+    {
+        throw std::exception();
+    }
+
+    ShowWindow(g_hWnd, nCmdShow);
+}
+
+
+// =============================================================================
+//
+// Init_Direct3D10_1
+//
+// =============================================================================
+
+void  Init_Direct3D10_1()
+{
+    // Создание фабрики DXGI, с помощью которой создаются различные объекты для
+    // работы с DirectX.
+
+    CHECK_HR( CreateDXGIFactory2(
+#ifdef  _DEBUG
+        DXGI_CREATE_FACTORY_DEBUG   |
+#endif
+        0,
+        IID_PPV_ARGS(&g_pIDXGI_Factory) ) );
+
+    // Получение теоретически самого высокопроизводительного адаптера в системе.
+    // Адаптер - это графический процессор (ГП) и всё, что обеспечивает его ра-
+    // боту, будь то отдельная видеокарта или же ГП, объединённый в одной микро-
+    // схеме с центральным процессором (ЦП).
+
+    CHECK_HR( g_pIDXGI_Factory->EnumAdapterByGpuPreference(
+        0,                                      // Порядковый номер адаптера
+        DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,   // Критерий выбора адаптера
+        IID_PPV_ARGS(&g_pDXGI_Adapter) ) );     // Объект адаптера
+
+    // Получение свойств выбранного адаптера. Реально это не требуется и здесь
+    // сделано, чтобы можно было визуально проконтролировать, тот ли адаптер
+    // был выбран.
+
+    DXGI_ADAPTER_DESC3  Adapter_Desc;
+
+    CHECK_HR( g_pDXGI_Adapter->GetDesc3(&Adapter_Desc) );
+
+    // Создание устройства Direct3D10.1 - т.е. объекта для взаимодействия с гра-
+    // фическим процессором (ГП), на котором будет выполняться рендеринг.
+
+    CHECK_HR( D3D10CreateDevice1(
+        g_pDXGI_Adapter,                    // pAdapter - какой ГП использовать
+        D3D10_DRIVER_TYPE_HARDWARE,         // Тип драйвера - реальное железо
+        NULL,                               // Software - всегда NULL
+#ifdef  _DEBUG
+        D3D10_CREATE_DEVICE_DEBUG   |
+#endif
+        D3D10_CREATE_DEVICE_SINGLETHREADED, // Flags
+        D3D10_FEATURE_LEVEL_10_1,           // HardwareLevel
+        D3D10_1_SDK_VERSION,                // SDKVersion
+        &g_pD3D_Device) );                  // ppDevice
+
+    // Создание цепочки переключения буферов, в которые будет осуществляться
+    // рендеринг и которые будут отображаться на экране. 
+    //
+    // В полноэкранном режиме необходимо иметь минимум два буфера - первичный и
+    // вторичный (содержимое первичного буфера отображается на мониторе, в то
+    // время как во вторичный производится рендеринг, т.е. формируется изображе-
+    // ние следующего кадра; когда рендеринг завершён, происходит переключение
+    // буферов: вторичный буфер становится первичным и тем самым отображается на
+    // экране, а первичный буфер становится вторичным, в котором будет формиро-
+    // ваться очередной кадр).
+    //
+    // В оконном режиме теоретически достаточно одного буфера, так как первичным
+    // буфером владеет сама ОС (упрощённо говоря, им будет рабочий стол), и кар-
+    // тинка из вторичного буфера каким-либо образом переносится в нужную часть
+    // первичного буфера. Однако в DirectX12 подобная модель уже не поддержива-
+    // ется, и в нём нужно иметь минимум два буфера.
+
+    DXGI_SWAP_CHAIN_DESC1  SCD;
+
+    SCD.Width               = Render_Width;
+    SCD.Height              = Render_Height;
+    SCD.Format              = DXGI_FORMAT_R8G8B8A8_UNORM;
+    SCD.Stereo              = false;
+    SCD.SampleDesc.Count    = 1;
+    SCD.SampleDesc.Quality  = 0;
+    SCD.BufferUsage         = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    SCD.BufferCount         = 2;
+    SCD.Scaling             = DXGI_SCALING_NONE;
+    SCD.SwapEffect          = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    SCD.AlphaMode           = DXGI_ALPHA_MODE_IGNORE;
+    SCD.Flags               = 0;
+
+    CHECK_HR( g_pIDXGI_Factory->CreateSwapChainForHwnd(
+        g_pD3D_Device,                          // pDevice
+        g_hWnd,                                 // hWnd
+        &SCD,                                   // pDesc
+        nullptr,                                // pFullscreenDesc
+        nullptr,                                // pRestrictToOutput
+        &g_pSwap_Chain) );                      // ppSwapChain
+
+    D3DX10_IMAGE_INFO       Info_From_File;
+    D3DX10_IMAGE_LOAD_INFO  Load_Image_Info;
+
+    ZeroMemory(&Info_From_File, sizeof(Info_From_File));
+    ZeroMemory(&Load_Image_Info, sizeof(Load_Image_Info));
+
+    CHECK_HR( D3DX10GetImageInfoFromFileW(
+        File_Name,
+        nullptr,
+        &Info_From_File,
+        nullptr) );
+
+    Load_Image_Info.Width           = Info_From_File.Width;
+    Load_Image_Info.Height          = Info_From_File.Height;
+    Load_Image_Info.Depth           = Info_From_File.Depth;
+    Load_Image_Info.FirstMipLevel   = 1;
+    Load_Image_Info.MipLevels       = Info_From_File.MipLevels;
+    Load_Image_Info.Usage           = D3D10_USAGE_DEFAULT;
+    Load_Image_Info.BindFlags       = D3D10_BIND_SHADER_RESOURCE;
+    Load_Image_Info.CpuAccessFlags  = 0;
+    Load_Image_Info.MiscFlags       = 0;
+    Load_Image_Info.Format          = Info_From_File.Format;
+    Load_Image_Info.Filter          = D3DX10_FILTER_NONE;
+    Load_Image_Info.MipFilter       = D3DX10_FILTER_NONE;
+    Load_Image_Info.pSrcInfo        = &Info_From_File;
+
+    CHECK_HR( D3DX10CreateShaderResourceViewFromFileW(
+        g_pD3D_Device,
+        File_Name,
+        &Load_Image_Info,
+        nullptr,
+        &g_pShader_Resource,
+        nullptr) );
+
+    CHECK_HR( D3DX10CreateSprite(g_pD3D_Device, 1, &g_pSprite) );
+}
+
+
+// =============================================================================
+//
+// Cleanup
+//
+// =============================================================================
+
+void  Cleanup()
+{
+    Safe_Release(g_pSwap_Chain);
+    Safe_Release(g_pD3D_Device);
+    Safe_Release(g_pDXGI_Adapter);
+    Safe_Release(g_pIDXGI_Factory);
+}
+
+
+// =============================================================================
+//
+// Render_Scene
+//
+// =============================================================================
+
+void  Render_Scene()
+{
+    // Определение цвета закраски окна.
+
+    const float  Clear_Color[4] = { 1.0f, 0.0f, 0.5f, 1.0f };
+
+    // Установка окна вывода (viewport), т.е. определение части цели рендеринга
+    // (буфера кадра), в которую будет осуществляться вывод изображения. Окно
+    // вывода используется на стадии растеризации, т.е. превращения трёхмерных
+    // примитивов (треугольников, линий и точек) в пиксели.
+
+    D3D10_VIEWPORT  VP;
+
+    VP.TopLeftX = 0;
+    VP.TopLeftY = 0;
+    VP.Width    = Render_Width;
+    VP.Height   = Render_Height;
+    VP.MinDepth = 0.0f;
+    VP.MaxDepth = 1.0f;
+
+    g_pD3D_Device->RSSetViewports(1, &VP);
+
+    // Создание представления цели рендеринга (render target view). Представле-
+    // ние - это механизм доступа к памяти, занимаемой некоторым ресурсом (целью
+    // рендеринга в данном случае); его нужно привязать к финальной стадии гра-
+    // фического конвейера, чтобы он знал, как и куда записывать окончательные
+    // результаты.
+    //
+    // Чтобы создать представление, сначала надо получить цель рендеринга, т.е.
+    // вторичный буфер. Текущий используемый вторичный буфер всегда имеет номер
+    // 0, поскольку Direct3D при переключении буферов меняет их местами.
+
+    ID3D10Resource          *pBack_Buffer;
+    ID3D10RenderTargetView  *pRender_Target_View;
+
+    CHECK_HR( g_pSwap_Chain->GetBuffer(0, IID_PPV_ARGS(&pBack_Buffer) ) );
+
+    CHECK_HR( g_pD3D_Device->CreateRenderTargetView(
+        pBack_Buffer,
+        nullptr,
+        &pRender_Target_View) );
+
+    Safe_Release(pBack_Buffer);
+
+    // Привязка представления к конвейеру и очистка буфера (закраска его задан-
+    // ным цветом).
+
+    g_pD3D_Device->OMSetRenderTargets(1, &pRender_Target_View, nullptr);
+
+    g_pD3D_Device->ClearRenderTargetView(pRender_Target_View, Clear_Color);
+
+    D3DXMATRIX  M_World;
+    D3DXMATRIX  M_View;
+    D3DXMATRIX  M_Projection;
+
+    D3DXMatrixTranslation(&M_World, 0.0f, 0.0f, 0.0f);
+
+    D3DXMatrixPerspectiveFovLH(
+        &M_Projection,
+        static_cast<float>(D3DX_PI) / 4.0f,
+        1.333f,
+        0.0f,
+        1.0f);
+
+    g_pSprite->SetProjectionTransform(&M_Projection);
+
+    D3DXVECTOR3  V_Eye_Point(0.0f, 0.0f, -3.0f);
+
+    D3DXVECTOR3  V_Lookat_Point(0.0f, 0.0f, 0.0f);
+
+    D3DXVECTOR3  V_Up_Vec(0.0f, 1.0f, 0.0f);
+
+    D3DXMatrixLookAtLH(&M_View, &V_Eye_Point, &V_Lookat_Point, &V_Up_Vec);
+
+    g_pSprite->SetViewTransform(&M_View);
+
+    g_pSprite->Begin(D3DX10_SPRITE_SORT_TEXTURE);
+
+    D3DX10_SPRITE  Sprite_To_Draw;
+
+    Sprite_To_Draw.matWorld = M_World;
+
+    Sprite_To_Draw.TexCoord.x = 0.0f;
+    Sprite_To_Draw.TexCoord.y = 0.0f;
+
+    Sprite_To_Draw.TexSize.x = 1.0f;
+    Sprite_To_Draw.TexSize.y = 1.0f;
+
+    Sprite_To_Draw.ColorModulate = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+
+    Sprite_To_Draw.pTexture = g_pShader_Resource;
+
+    Sprite_To_Draw.TextureIndex = 0;
+
+    CHECK_HR( g_pSprite->DrawSpritesBuffered(&Sprite_To_Draw, 1) );
+
+    g_pSprite->Flush();
+    g_pSprite->End();
+
+    // Вывод сформированного изображения (переключение буферов).
+
+    g_pSwap_Chain->Present(0, DXGI_PRESENT_RESTART);
+
+    Safe_Release(pRender_Target_View);
+}
+
+
+// =============================================================================
+//
+// wWinMain
+//
+// =============================================================================
+
+int WINAPI  wWinMain(
+    HINSTANCE  hInstance,
+    HINSTANCE,
+    PWSTR,
+    int        nCmdShow)
+{
+    Init_Window(hInstance, nCmdShow);
+
+    Init_Direct3D10_1();
+
+    MSG  Message = {};
+
+    while ( Message.message != WM_QUIT )
+    {
+        if ( PeekMessageW(&Message, NULL, 0, 0, PM_REMOVE) )
+        {
+            TranslateMessage(&Message);
+            DispatchMessageW(&Message);
+        }
+        else
+        {
+            Render_Scene();
+        }
+    }
+
+    Cleanup();
+
+    return static_cast<int>(Message.wParam);
+}
