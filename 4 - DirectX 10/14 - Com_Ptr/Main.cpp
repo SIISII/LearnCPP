0cd9e7b678834c9552ddc15664881c73fb24340a
AtshelnikE <alexei.evkarpiev@yandex.ru>
2021-04-26 20:35:44 +0300

    Example 14 started

---
+++ 4 - DirectX 10/14 - Com_Ptr/Main.cpp
@@ -0,0 +1,94 @@
+#include "Common_Defs.h"
+
+THIRD_PARTY_START
+
+#include  <Windows.h>
+#include  <cstdio>
+
+#include  <DXGI1_6.h>
+
+THIRD_PARTY_END
+
+template<class  T>
+class  Com_Ptr
+{
+private:
+
+    T  *Ptr = nullptr;
+
+public:
+
+    ~Com_Ptr()
+    {
+        if (Ptr != nullptr)
+        {
+            Ptr->release();
+        }
+    }
+
+    void** Get_Addr() const
+    {
+        if (Ptr != nullptr)
+        {
+            throw std::exception();
+        }
+        return &Ptr;
+    }
+
+    T* operator->() const noexcept
+    {
+        return Ptr;
+    }
+};
+
+FILE           *F;
+
+Com_Ptr<IDXGIFactory7>  g_pDXGI_Factory;
+Com_Ptr<IDXGIAdapter4>  g_pDXGI_Adapter;
+
+
+int WINAPI  wWinMain(
+    HINSTANCE,
+    HINSTANCE,
+    PWSTR,
+    int)
+{
+    // Создание фабрики DXGI, с помощью которой создаются различные объекты для
+    // работы с DirectX.
+    CreateDXGIFactory2(0, IID_IDXGIFactory2, g_pDXGI_Factory.Get_Addr() );
+
+    fopen_s(&F, "Adapters.txt", "w+");
+
+    uint32   N = 0;
+    HRESULT  HR;
+
+    do
+    {
+        // Получение адаптера с номером N. Если его нет, в HR будет помещён код
+        // ошибки, иначе - код успеха S_OK.
+        HR = g_pDXGI_Factory->EnumAdapterByGpuPreference(
+            N,                                      // Порядковый номер адаптера
+            DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,   // Критерий выбора адаптера
+            IID_IDXGIAdapter4,
+            g_pDXGI_Adapter.Get_Addr());       // Объект адаптера
+
+        // Если адаптер успешно получен, запись его имени в файл и освобождение
+        // самого адаптера.
+        if ( SUCCEEDED(HR) )
+        {
+            DXGI_ADAPTER_DESC3  Desc;
+
+            g_pDXGI_Adapter->GetDesc3(&Desc);
+
+            fwprintf_s(F, L"%s\n", Desc.Description);
+
+            g_pDXGI_Adapter->Release();
+        }
+
+        ++N;
+    } while ( SUCCEEDED(HR) );
+    
+    fclose(F);
+
+    g_pDXGI_Factory->Release();
+}
