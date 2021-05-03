cf3c8978bcc8fbacd34b7e20e5032c4fe688691e
Ivan Savvateev <ivansavvateev@yandex.ru>
2021-05-03 20:37:48 +0300

    Small corrs

--- 4 - DirectX 10/15 - Mode List/Main.cpp
+++ 4 - DirectX 10/15 - Mode List/Main.cpp
@@ -5,15 +5,13 @@ THIRD_PARTY_START
 
 #include  <cassert>
 #include  <cstdio>
+#include  <clocale>
 
 #include  <Windows.h>
 
 #include  <DXGI1_6.h>
 
-#include <clocale>
-
 THIRD_PARTY_END
-
 
 
 FILE  *F;
@@ -48,8 +46,13 @@ int WINAPI  wWinMain(
             DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,   // Критерий выбора адаптера
             IID_PPV_ARGS(pDXGI_Adapter.Get_Addr()) );     // Объект адаптера
 
-        // Если адаптер успешно получен, запись его имени в файл и освобождение
-        // самого адаптера.
+        // Если адаптер успешно получен, запись его имени в файл, а затем пере-
+        // числение его выходов. Для каждого выхода получается и выводится в
+        // файл список поддерживаемых режимов вывода.
+        //
+        // Освобождать интерфейсы явным образом не требуется, поскольку для них
+        // применяются умные указатели (Com_Ptr): они сами знают, когда и как
+        // освобождать интерфейсы.
         if ( SUCCEEDED(HR) )
         {
             DXGI_ADAPTER_DESC3  Desc;
@@ -79,37 +82,52 @@ int WINAPI  wWinMain(
                     {
                         uint32  Num;
 
-                        const uint32 Flags = DXGI_ENUM_MODES_INTERLACED | DXGI_ENUM_MODES_SCALING | DXGI_ENUM_MODES_STEREO | DXGI_ENUM_MODES_DISABLED_STEREO;
+                        const uint32  Flags = DXGI_ENUM_MODES_INTERLACED    |
+                                              DXGI_ENUM_MODES_SCALING       |
+                                              DXGI_ENUM_MODES_STEREO        |
+                                              DXGI_ENUM_MODES_DISABLED_STEREO;
 
-                        HR3 = pDXGI_Output1->GetDisplayModeList1(DXGI_FORMAT_R8G8B8A8_UNORM, Flags, &Num, nullptr);
+                        // Определение количества видеорежимов.
+                        HR3 = pDXGI_Output1->GetDisplayModeList1(
+                            DXGI_FORMAT_R8G8B8A8_UNORM,
+                            Flags,
+                            &Num,
+                            nullptr);
 
+                        // Выделение памяти под массив описателей видеорежимов.
                         DXGI_MODE_DESC1  *Ptr = new DXGI_MODE_DESC1[Num];
 
-                        HR3 = pDXGI_Output1->GetDisplayModeList1(DXGI_FORMAT_R8G8B8A8_UNORM, Flags, &Num, Ptr);
+                        // Получение описателей видеорежимов.
+                        HR3 = pDXGI_Output1->GetDisplayModeList1(
+                            DXGI_FORMAT_R8G8B8A8_UNORM,
+                            Flags,
+                            &Num,
+                            Ptr);
 
+                        // Вывод информации о видеорежимах.
                         for ( uint32  I = 0; I < Num; ++I )
                         {
                             fwprintf_s(F, L"    %i x %i ", Ptr[I].Width, Ptr[I].Height);
 
-                            if (Ptr[I].RefreshRate.Numerator % Ptr[I].RefreshRate.Denominator == 0)
+                            if ( Ptr[I].RefreshRate.Numerator % Ptr[I].RefreshRate.Denominator == 0 )
                             {
                                 fwprintf_s(F, L"@ %i Hz ", Ptr[I].RefreshRate.Numerator / Ptr[I].RefreshRate.Denominator);
                             }
                             else
                             {
-                                float Res = static_cast<float>(Ptr[I].RefreshRate.Numerator) / static_cast<float>(Ptr[I].RefreshRate.Denominator);
+                                float  Res = static_cast<float>(Ptr[I].RefreshRate.Numerator) / static_cast<float>(Ptr[I].RefreshRate.Denominator);
 
                                 fwprintf_s(F, L"@ %7.3f Hz ", Res);
                             }
 
                             fwprintf_s(F, L"R8G8B8A8 ");
 
-                            if (Ptr[I].ScanlineOrdering !=  DXGI_MODE_SCANLINE_ORDER_PROGRESSIVE)
+                            if ( Ptr[I].ScanlineOrdering !=  DXGI_MODE_SCANLINE_ORDER_PROGRESSIVE )
                             {
                                 fwprintf_s(F, L"чересстрочно ");
                             }
 
-                            switch (Ptr[I].Scaling)
+                            switch ( Ptr[I].Scaling )
                             {
                                 case DXGI_MODE_SCALING_CENTERED:
                                     fwprintf_s(F, L"центрирование ");
@@ -123,7 +141,7 @@ int WINAPI  wWinMain(
                                     break;
                             }
 
-                            if (Ptr[I].Stereo)
+                            if ( Ptr[I].Stereo )
                             {
                                  fwprintf_s(F, L"Стерео");
                             }
@@ -131,6 +149,9 @@ int WINAPI  wWinMain(
                             fwprintf_s(F, L"\n");
                         }
 
+                        // Освобождение памяти, выделенной под описатели видео-
+                        // режимов. Поскольку используется обычный, а не умный
+                        // указатель, освобождение нужно делать явным образом.
                         delete [] Ptr;
                     }
                 }
