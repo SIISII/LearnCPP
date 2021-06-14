30d9dbecd854461f29e62566e029f395c581b869
Ivan Savvateev <ivansavvateev@yandex.ru>
2021-06-14 18:05:19 +0300

    Example 14 completed:

--- 3 - Windows/Project14 - Async IO/Source.cpp
+++ 3 - Windows/Project14 - Async IO/Source.cpp
@@ -3,10 +3,15 @@
 #include  <cstdlib>
 #include  <cstdio>
 
+constexpr int  Num_Numbers = 500;
+
 HANDLE  F;
 HANDLE  E;
-constexpr int  Num_Numbers = 500;
+
 int  Counter = 0;
+
+const char  Dummy[16384] {};
+
 
 int  main()
 {
@@ -17,38 +22,41 @@ int  main()
     E = CreateEvent(nullptr, true, false, nullptr);
 
     F = CreateFileW(
-        L"D:\\File.txt",
+        L"File.txt",
         GENERIC_WRITE,
         FILE_SHARE_READ,
         nullptr,
         CREATE_ALWAYS,
-        FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED,
+        FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED | FILE_FLAG_WRITE_THROUGH,
         NULL);
 
-    OVERLAPPED  Ov = {};
+    OVERLAPPED  Ov {};
     Ov.hEvent = E;
 
-    int Counter = 0;
+    int  Counter = 0;
 
     for ( int  I = 0; I < Num_Numbers; ++I )
     {
-        char  S[1000000];
+        char  S[16];
         sprintf_s(S, sizeof(S), "%i\n", rand());
-        //int  Len = strlen(S);
-        int  Len = 1000000;
+        int  Len = strlen(S);
+
         WriteFile(
             F,
             S,
             Len,
             nullptr,
             &Ov);
-        while ( WaitForSingleObject(E, INFINITE) != WAIT_OBJECT_0 )
+
+        while ( WaitForSingleObject(E, 0) != WAIT_OBJECT_0 )
         {
             ++Counter;
         }
+
         Ov.Offset = Ov.Offset + Len;
         ResetEvent(E);
     }
+
     printf("%i", Counter);
 
     CloseHandle(F);
