906078b4d41fbe39bb453cbb72007beefbd444fa
Ivan Savvateev <ivansavvateev@yandex.ru>
2021-06-18 04:00:00 +0300

    Example 16 completed

--- 3 - Windows/Project16 - Thread Pool/Source.cpp
+++ 3 - Windows/Project16 - Thread Pool/Source.cpp
@@ -1,77 +1,64 @@
 #include  <cstdio>
 #include  <clocale>
-#include  <thread>
-#include  <Windows.h>
-#include  <vector>
-#include  <queue>
-#include  <mutex>
 #include  <cstdlib>
-#include  <atomic>
+#include  <Windows.h>
 
 constexpr int  Num_Threads = 10;
 constexpr int  Num_Numbers = 500;
 
 
-void  Second_Thread();
+void CALLBACK  Work_Callback_Proc(
+    PTP_CALLBACK_INSTANCE  Instance,
+    PVOID                  Context,
+    PTP_WORK               Work);
 
 
-PTP_POOL  Pool;
 TP_CALLBACK_ENVIRON  CB_Env {};
 
-std::queue<int>             Queue;
-std::mutex                  Mutex;
-std::atomic_int             Counter = 0;
-std::atomic_int             Print_Counter = 0;
+PTP_POOL             Pool          = nullptr;
+PTP_CLEANUP_GROUP    Cleanup_Group = nullptr;
 
 
 int  main()
 {
     std::setlocale(LC_ALL, "Russian");
-    
-    srand(478);
+
+    InitializeThreadpoolEnvironment(&CB_Env);
 
     Pool = CreateThreadpool(nullptr);
+
     SetThreadpoolThreadMaximum(Pool, 20);
     SetThreadpoolThreadMinimum(Pool, 5);
 
-    InitializeThreadpoolEnvironment(&CB_Env);
     SetThreadpoolCallbackPool(&CB_Env, Pool);
+
+    Cleanup_Group = CreateThreadpoolCleanupGroup();
+
+    SetThreadpoolCallbackCleanupGroup(&CB_Env, Cleanup_Group, nullptr);
 
     for ( int  I = 0; I < Num_Numbers; ++I )
     {
-        Mutex.lock();
-        Queue.push(rand());
-        Mutex.unlock();
+        PTP_WORK  Work = CreateThreadpoolWork(
+            Work_Callback_Proc,
+            reinterpret_cast<void*>(I),
+            &CB_Env);
+        SubmitThreadpoolWork(Work);
     }
 
-    while ( Print_Counter == Num_Numbers );
+    CloseThreadpoolCleanupGroupMembers(Cleanup_Group, false, nullptr);
 }
 
 
-void  Second_Thread()
+void CALLBACK  Work_Callback_Proc(
+    PTP_CALLBACK_INSTANCE  Instance,
+    PVOID                  Context,
+    PTP_WORK               Work)
 {
-    int  Thread_Num = Counter++;
-    
-    while ( true )
-    {
-        int   Cur_Num;
-        bool  Present = false;
+    int  I = reinterpret_cast<int>(Context);
 
-        Mutex.lock();
+    DWORD  Id = GetCurrentThreadId();
 
-        if ( !Queue.empty() )
-        {
-            Present = true;
-            Cur_Num = Queue.front();
-            Queue.pop();
-        }
+    printf("Thread = %08X; Number = %i\n", Id, I);
 
-        Mutex.unlock();
-
-        if ( Present )
-        {
-            printf_s("%i %i \n", Cur_Num, Thread_Num);
-            ++Print_Counter;
-        }
-    }
+    Sleep(1000);
 }
